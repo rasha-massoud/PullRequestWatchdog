9930feb70e98d14707ffbc84580859913c212e29
RashaM <rashamassoud1998@gmail.com>
2023-05-20 23:50:19 +0300

    Added the routes to the previous created APIs

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\PullRequestController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(["prefix" => "pull-requests"], function(){
+
+    Route::get('/old', [PullRequestController::class, 'getOldPullRequests']);
+    Route::get('/review-required', [PullRequestController::class, 'getPullRequestsWithReviewRequired']);
+    Route::get('/review-successful', [PullRequestController::class, 'getPullRequestsWithSuccessfulReview']);
+    Route::get('/no-reviews-requested', [PullRequestController::class, 'getPullRequestsWithNoReviewsRequested']);
 });
