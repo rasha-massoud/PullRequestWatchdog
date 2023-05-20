b243234deb4a4e104f179802c2a997cfa72257b7
RashaM <rashamassoud1998@gmail.com>
2023-05-20 21:27:15 +0300

    Created the method to fetch the list of open pull requests created more than 14 days ago and the method to write the details to a text file

--- app/Http/Controllers/PullRequestController.php
+++ app/Http/Controllers/PullRequestController.php
@@ -4,7 +4,38 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Carbon\Carbon;
+use GuzzleHttp\Client;
+
 class PullRequestController extends Controller
 {
-    //
+    public function getOldPullRequests()
+    {
+        $client = new Client();
+        $twoWeeksAgo = Carbon::now()->subDays(14)->format('Y-m-d');
+
+        $response = $client->get('https://api.github.com/search/issues', [
+            'query' => [
+                'q' => 'repo:woocommerce/woocommerce is:pr is:open created:<'.$twoWeeksAgo,
+                'per_page' => 100,
+            ],
+        ]);
+
+        $pullRequests = json_decode($response->getBody(), true)['items'];
+
+        $this->writePullRequestsToFile('1-old-pull-requests.txt', $pullRequests);
+    }
+
+    private function writePullRequestsToFile($filename, $pullRequests)
+    {
+        $file = fopen($filename, 'w');
+
+        foreach ($pullRequests as $pullRequest) {
+            $line = 'PR#' . $pullRequest['number'] . ': ' . $pullRequest['title'] . ' (' . $pullRequest['html_url'] . ')';
+            fwrite($file, $line . PHP_EOL);
+        }
+
+        fclose($file);
+    }
+
 }
