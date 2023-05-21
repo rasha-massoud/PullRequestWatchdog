78b03c6f50eeba9e3ceac7a6474fe31cf7e37dc0
RashaM <rashamassoud1998@gmail.com>
2023-05-21 10:40:02 +0300

    Updated the code to include error and exception handlings

--- app/Http/Controllers/PullRequestController.php
+++ app/Http/Controllers/PullRequestController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
+use Exception;
 use Carbon\Carbon;
 use GuzzleHttp\Client;
 
@@ -11,67 +13,89 @@ class PullRequestController extends Controller
 {
     public function getOldPullRequests()
     {
-        $client = new Client();
-        $twoWeeksAgo = Carbon::now()->subDays(14)->format('Y-m-d');
+        try {
 
-        $response = $client->get('https://api.github.com/search/issues', [
-            'query' => [
-                'q' => 'repo:woocommerce/woocommerce is:pr is:open created:<'.$twoWeeksAgo,
-                'per_page' => 100,
-            ],
-        ]);
-
-        $pullRequests = json_decode($response->getBody(), true)['items'];
-
-        $this->writePullRequestsToFile('1-old-pull-requests.txt', $pullRequests);
+            $client = new Client();
+            $twoWeeksAgo = Carbon::now()->subDays(14)->format('Y-m-d');
+    
+            $response = $client->get('https://api.github.com/search/issues', [
+                'query' => [
+                    'q' => 'repo:woocommerce/woocommerce is:pr is:open created:<'.$twoWeeksAgo,
+                    'per_page' => 100,
+                ],
+            ]);
+    
+            $pullRequests = json_decode($response->getBody(), true)['items'];
+    
+            $this->writePullRequestsToFile('1-old-pull-requests.txt', $pullRequests);
+        
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'An error has occured while getting old pull requests']);
+        }
+       
     }
 
     public function getPullRequestsWithReviewRequired()
     {
-        $client = new Client();
+        try{
+            $client = new Client();
 
-        $response = $client->get('https://api.github.com/search/issues', [
-            'query' => [
-                'q' => 'repo:woocommerce/woocommerce is:pr is:open review:required',
-                'per_page' => 100,
-            ],
-        ]);
+            $response = $client->get('https://api.github.com/search/issues', [
+                'query' => [
+                    'q' => 'repo:woocommerce/woocommerce is:pr is:open review:required',
+                    'per_page' => 100,
+                ],
+            ]);
 
-        $pullRequests = json_decode($response->getBody(), true)['items'];
+            $pullRequests = json_decode($response->getBody(), true)['items'];
 
-        $this->writePullRequestsToFile('2-review-required-pull-requests.txt', $pullRequests);
+            $this->writePullRequestsToFile('2-review-required-pull-requests.txt', $pullRequests);
+            
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'An error has occured while getting old pull requests with review required']);
+        }
     }
 
     public function getPullRequestsWithSuccessfulReview()
     {
-        $client = new Client();
+        try{
+            $client = new Client();
 
-        $response = $client->get('https://api.github.com/search/issues', [
-            'query' => [
-                'q' => 'repo:woocommerce/woocommerce is:pr is:open status:success',
-                'per_page' => 100,
-            ],
-        ]);
+            $response = $client->get('https://api.github.com/search/issues', [
+                'query' => [
+                    'q' => 'repo:woocommerce/woocommerce is:pr is:open status:success',
+                    'per_page' => 100,
+                ],
+            ]);
 
-        $pullRequests = json_decode($response->getBody(), true)['items'];
+            $pullRequests = json_decode($response->getBody(), true)['items'];
 
-        $this->writePullRequestsToFile('3-review-successful-pull-requests.txt', $pullRequests);
+            $this->writePullRequestsToFile('3-review-successful-pull-requests.txt', $pullRequests);
+
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'An error has occured while getting old pull requests with successful review']);
+        }
     }
 
     public function getPullRequestsWithNoReviewsRequested()
     {
-        $client = new Client();
+        try{
+            $client = new Client();
 
-        $response = $client->get('https://api.github.com/search/issues', [
-            'query' => [
-                'q' => 'repo:woocommerce/woocommerce is:pr is:open -review:none',
-                'per_page' => 100,
-            ],
-        ]);
+            $response = $client->get('https://api.github.com/search/issues', [
+                'query' => [
+                    'q' => 'repo:woocommerce/woocommerce is:pr is:open -review:none',
+                    'per_page' => 100,
+                ],
+            ]);
 
-        $pullRequests = json_decode($response->getBody(), true)['items'];
+            $pullRequests = json_decode($response->getBody(), true)['items'];
 
-        $this->writePullRequestsToFile('4-no-reviews-requested-pull-requests.txt', $pullRequests);
+            $this->writePullRequestsToFile('4-no-reviews-requested-pull-requests.txt', $pullRequests);
+            
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'An error has occured while getting old pull requests with no requested review']);
+        }
     }
 
     private function writePullRequestsToFile($filename, $pullRequests)
