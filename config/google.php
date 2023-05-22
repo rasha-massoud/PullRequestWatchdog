7c30877158d50dcc388601b5de28870f79af45fe
RashaM <rashamassoud1998@gmail.com>
2023-05-22 22:26:57 +0300

    Created the google.php file

---
+++ config/google.php
@@ -0,0 +1,23 @@
+<?php
+    return [
+
+        'application_name' => env('GOOGLE_APPLICATION_NAME', ''),
+
+        'client_id'        => env('GOOGLE_CLIENT_ID', ''),
+        'client_secret'    => env('GOOGLE_CLIENT_SECRET', ''),
+        'redirect_uri'     => env('GOOGLE_REDIRECT', ''),
+        'scopes'           => [\Google_Service_Sheets::DRIVE, \Google_Service_Sheets::SPREADSHEETS],
+        'access_type'      => 'online',
+        'approval_prompt'  => 'force',
+        'prompt'           => 'consent', 
+
+        'developer_key'    => env('GOOGLE_DEVELOPER_KEY', ''),
+
+        'service'          => [
+            'enable' => env('GOOGLE_SERVICE_ENABLED', true),
+            'file'   => env('GOOGLE_SERVICE_ACCOUNT_JSON_LOCATION', storage_path('app/githubpullrequest-f00e90674ef4.json')),
+        ],
+        'config'           => [],
+        'post_spreadsheet_id' => env('POST_SPREADSHEET_ID'),
+        'post_sheet_id'       => env('POST_SHEET_ID'),
+    ];
